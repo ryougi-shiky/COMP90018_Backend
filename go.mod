702fbd505f0ffac76a70cc0f334b74156a63cd21
RyougiShiki <805482013@qq.com>
2023-07-29 17:42:58 +1000

    switched to dev branch

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ryougi-shiky/COMP90018_Backend
+
+go 1.20
+
+require github.com/google/uuid v1.3.0 // indirect
--- module/users.go
+++ module/users.go
@@ -1,9 +1,12 @@
 package models
 
-import {
+import (
 	"github.com/google/uuid"
-}
+)
 
 type User struct {
 	ID uuid.UUID `gorm:"type:char(36);primary_key"`
+	username string `gorm:"type:varchar(20);not null`
+	Email string `gorm:"type:varchar(40);not null`
+	Password string `gorm:"type:char(64);not null`
 }
